21463563d538610e47640bd9624d2ca23bf7ec05
Carlo DiCelico <carlodicelico@me.com>
2015-10-28 12:23:17 -0400

    convert to feet by default

--- conversions.ex
+++ conversions.ex
@@ -1,7 +1,13 @@
 defmodule Conversions do
   defmodule MetersToLength do
+    def convert(m) when is_number(m) and m > 0, do: convert(:feet, m)
     def convert(:feet, m) when is_number(m) and m > 0, do: m * 3.28084
     def convert(:inch, m) when is_number(m) and m > 0, do: m * 39.3701
     def convert(:yard, m) when is_number(m) and m > 0, do: m * 1.09361
   end
 end
+
+# equivalent
+# Conversions.MetersToLength.convert(:feet, MathUtils.cb(MathUtils.sq(MathUtils.fac(4))))
+# MathUtils.fac(4) |> MathUtils.sq |> MathUtils.cb |> Conversions.MetersToLength.convert:w
+# 626978287.77984
